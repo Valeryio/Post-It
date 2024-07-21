8a6dc3989fe3aa168925b13456e5cc4a0cb2d02a
Valeryio <tommagoma23@gmail.com>
2024-07-21 15:59:15 +0100

    Adding some comments
    Some simple explicative comments have been added!

---
+++ config.js
@@ -0,0 +1,4 @@
+let container = document.querySelector(".container");
+let posterText = document.querySelector(".poster-text");
+let activeInput = document.querySelector(".active_input");
+let oldValue = "";
--- script.js
+++ script.js
@@ -1,22 +1,31 @@
-let container = document.querySelector(".container");
-let posterText = document.querySelector(".poster-text");
-let activeInput = document.querySelector(".active_input");
-let oldValue = "";
+
+/**
+ * This first bloc of code act when you click on the poster's
+ * content
+ * 
+ * When you click on it, the poster goes out the flux and the
+ * input takes the entry you gave.
+ */
 
 posterText.addEventListener("click", (event) => {
 
   posterText.style.display= "none";
   let oldValue = posterText.textContent;
-  console.log(oldValue);
 
   activeInput.style.display= "block";
 });
 
+/**
+ * This second block is triggered when your mouse got out
+ * the bloc
+ * 
+ * Once you've finished to write you new post content, then
+ * the content of the poster-text paragraph is changed and
+ * displayed at its first place!
+ */
 activeInput.addEventListener("mouseout", (event) => {
-//   console.log("The value: ", event.target.value);
-//   console.log("The type :", typeof(event.target.value));
-//   console.log("The old value :", oldValue);
 
+  // Keep the old value of the poster if the new post is empty
   if (event.target.value == "")
   {
     activeInput.style.display = "None";
@@ -24,6 +33,7 @@ activeInput.addEventListener("mouseout", (event) => {
   }
   else {
     posterText.textContent = event.target.value;
+    activeInput.placeholder = event.target.value;
     activeInput.style.display = "None";
     posterText.style.display = "block";
   }
